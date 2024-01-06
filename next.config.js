aa892f62ac6b3d258a365ad119fbb11cbb2889bc
Nivedita Gudavalli <111736292+nivedita112@users.noreply.github.com>
2024-01-07 01:47:35 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig}**/
 const nextConfig = {
+  "output: export"
   basePath: '/portfolio', // replace with your GitHub repository name
   assetPrefix: '/portfolio/', // replace with your GitHub repository name
 };
