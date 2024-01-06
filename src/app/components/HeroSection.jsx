69f47ebc6922cc1508cdd535ec1dd705f69c0a65
Nivedita Gudavalli <111736292+nivedita112@users.noreply.github.com>
2024-01-07 02:35:27 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -66,7 +66,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="https://github.com/nivedita112/portfolio/blob/main/public/images/hero-image.png?raw=true"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={350}
