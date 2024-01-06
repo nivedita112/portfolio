8f3cdc35b502a9a767ded2dcf62d0056d4f441d0
Nivedita Gudavalli <111736292+nivedita112@users.noreply.github.com>
2024-01-07 00:37:16 +0530

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <div className="text-2xl md:text-5xl text-white font-semibold">
-          <Image src="/images/Logo.gif" width={100} height={100} />
+          <Image src="/images/Logo.gif" width={100} height={100} alt = "LOGO" />
         </div>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
