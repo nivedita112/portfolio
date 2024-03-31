0b27b59ee1d97b414327c33f2bd7d387b368e3a8
Nivedita Gudavalli <111736292+nivedita112@users.noreply.github.com>
2024-03-31 19:58:14 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span className="text-transparent bg-clip-text">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -43,13 +43,13 @@ const HeroSection = () => {
           <div>
             <a
               href="mailto:niveditagudavalli112@gmail.com"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:shadow-lg hover:scale-105 transition-all duration-300 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4  hover:shadow-lg hover:scale-105 transition-all duration-300 text-white"
             >
               Hire Me
             </a>
             <a
               href="https://drive.google.com/file/d/124A4w2WvqGJEbvxC9p8vRl7KzP3HZHwX/view"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:shadow-lg hover:scale-105 transition-all duration-300 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full hover:shadow-lg hover:scale-105 transition-all duration-300 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 View CV
