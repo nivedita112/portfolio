11892f0fb1d4b4df15aaf4a718ca096214c794fa
Nivedita Gudavalli <111736292+nivedita112@users.noreply.github.com>
2024-04-01 09:59:52 +0530

    final update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
-import ProjectsSection from "./components/ProjectsSection";
+import CertificatesSection from "./components/CertificatesSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
@@ -18,7 +18,7 @@ export default function Home() {
         <AchievementsSection />
         <AboutSection />
         <InterestsSection/>
-        <ProjectsSection />
+        <CertificatesSection />
         <EmailSection />
       </div>
       <Footer />
